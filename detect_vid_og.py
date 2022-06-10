750fe2570943fe17b29707c49474fac5a06eb0a2
Sergio Hernandez <shc010013@gmail.com>
2022-06-09 22:22:34 -0500

    Added comments

--- detect_vid_og.py
+++ detect_vid_og.py
@@ -4,17 +4,18 @@ import cv2
 import time
 import numpy as np
 
+# Create a VideoCapture object to read from the given video file
 cap = cv2.VideoCapture('input/video_1.mp4')
 
-# get the video frames' width and height for proper saving of videos
+# Get the video frames' width and height for proper saving of videos
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
 
-# create the `VideoWriter()` object
+# Create a VideoWriter object to write an output video with the given specs
 out = cv2.VideoWriter('output/video_result_1.mp4',
                       cv2.VideoWriter_fourcc(*'mp4v'), 30, (frame_width, frame_height))
 
-# detect objects in each frame of the video
+# Detect objects in each frame of the video
 while cap.isOpened():
     ret, frame = cap.read()
 
@@ -22,8 +23,9 @@ while cap.isOpened():
         image = frame
 
         cv2.imshow('image', image)
-        out.write(image)
+        out.write(image)  # Write the frame into the output video file
 
+        # Press 'q' to exit
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
@@ -31,4 +33,6 @@ while cap.isOpened():
         break
 
 cap.release()
+# out.release()
+
 cv2.destroyAllWindows()
