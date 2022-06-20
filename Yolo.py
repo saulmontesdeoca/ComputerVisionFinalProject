cbe8978ddc49aff4d1d6aea9e6862ba81f6e3f48
Sergio Hernandez Castillo <shc010013@gmail.com>
2022-06-19 21:10:02 -0500

    Changes

--- Yolo.py
+++ Yolo.py
@@ -24,7 +24,7 @@ ap.add_argument("-v", "--video", required=True, help="Archivo de video")
 args = ap.parse_args()
 
 
-cap = cv2.VideoCapture('input/' + args.video)
+cap = cv2.VideoCapture("input/" + args.video)
 
 # get the video frames' width and height for proper saving of videos
 frame_width = int(cap.get(3))
@@ -45,23 +45,34 @@ while cap.isOpened():
     if ret:
         image = frame
 
+        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        rgb_image = cv2.resize(rgb_image, (700, 700))
+
+        R, G, B = cv2.split(rgb_image)
         # equalize the image
-        colorimage_r = cv2.equalizeHist(image[:, :, 2])
-        colorimage_g = cv2.equalizeHist(image[:, :, 1])
-        colorimage_b = cv2.equalizeHist(image[:, :, 0])
+        colorimage_r = cv2.equalizeHist(R)
+        colorimage_g = cv2.equalizeHist(G)
+        colorimage_b = cv2.equalizeHist(B)
 
         # merge the channels
-        colorimage_e = cv2.merge((colorimage_b, colorimage_g, colorimage_r))
+        colorimage_merge = cv2.merge(
+            (colorimage_r, colorimage_g, colorimage_b))
 
+        h, w = colorimage_merge.shape[:2]
+
+        red_values_mean = np.mean(colorimage_merge[:, :, 0])
+        green_values_mean = np.mean(colorimage_merge[:, :, 1])
+        blue_values_mean = np.mean(colorimage_merge[:, :, 2])
         (
             image_height,
             image_width,
             _,
-        ) = colorimage_e.shape  # get height and width of image
+        ) = colorimage_merge.shape  # get height and width of image
 
         # add blob to the preprocessed image
         blob = cv2.dnn.blobFromImage(
-            colorimage_e, 1 / 255.0, (416, 416), crop=False)
+            colorimage_merge, 1 / 255.0, (416, 416), crop=False
+        )
         yolo_model.setInput(blob)
         layerOutputs = yolo_model.forward(ln)
 
@@ -81,7 +92,8 @@ while cap.isOpened():
                 confidence = scores[classID]
                 # filter out weak predictions by ensuring the detected
                 # probability is greater than the minimum probability
-                if confidence > 0.7:
+                if confidence > 0.5:
+                    print(confidence)
                     # scale the bounding box coordinates back relative to the
                     # size of the image, keeping in mind that YOLO actually
                     # returns the center (x, y)-coordinates of the bounding
@@ -111,10 +123,12 @@ while cap.isOpened():
                 (w, h) = (boxes[i][2], boxes[i][3])
                 # draw a bounding box rectangle and label on the image
                 color = [int(c) for c in COLORS[classIDs[i]]]
-                cv2.rectangle(colorimage_e, (x, y), (x + w, y + h), color, 2)
+                cv2.rectangle(colorimage_merge, (x, y),
+                              (x + w, y + h), color, 2)
                 text = f"{class_names[classIDs[i]]}"
+
                 cv2.putText(
-                    colorimage_e,
+                    colorimage_merge,
                     text,
                     (x, y - 5),
                     cv2.FONT_HERSHEY_SIMPLEX,
@@ -122,9 +136,18 @@ while cap.isOpened():
                     color,
                     2,
                 )
-
-        cv2.imshow("image", colorimage_e)
-        out.write(colorimage_e)
+                cv2.putText(
+                    colorimage_merge,
+                    str(round(confidences[i], 2)),
+                    (int(x + 80), int(y - 5)),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    color,
+                    2,
+                )
+        bgr_image = cv2.cvtColor(colorimage_merge, cv2.COLOR_BGR2RGB)
+        cv2.imshow("image", bgr_image)
+        out.write(colorimage_merge)
         if cv2.waitKey(10) & 0xFF == ord("q"):
             break
     else:
